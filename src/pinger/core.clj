91e22145678bde5924750ed95182521329d6f5e5
Daniel Lobato <elobatocs@gmail.com>
2015-07-05 16:26:39 -0400

    Return the least pinged person in the collection

--- src/pinger/core.clj
+++ src/pinger/core.clj
@@ -14,7 +14,7 @@
                      (read-string (second raw-person-array)))}))
 
 (defn file-to-line-seq
-  "Creates a line-seq from a file"
+  "Create a line-seq from a file"
   [file]
   (line-seq (try (io/reader file)
               (catch java.io.FileNotFoundException exception
@@ -22,14 +22,21 @@
                   (. System exit 1))))))
 
 (defn read-people
-  "Creates a sequence of people to ping"
+  "Create a sequence of people to ping"
   [people-filepath]
   (map initialize-person (file-to-line-seq people-filepath)))
+
+(defn find-least-pinged
+  "Given a map with names and number of pings, find the group of people that has
+  been pinged the least"
+  [people]
+  (let [least-pinged (apply min (map :times-pinged people))]
+    (filter (fn [x] (= (:times-pinged x) least-pinged)) people)))
 
 (defn -main
   "Pick randomly someone to ping in the list of people"
   [& args]
   (if (== 1 (count args))
-    (println "ping" (:name (rand-nth (read-people (first args)))))
+    (println "ping" (:name (rand-nth (find-least-pinged
+                                       (read-people (first args))))))
     (println "Usage: pinger FILE")))
-
--- test/pinger/core_test.clj
+++ test/pinger/core_test.clj
@@ -10,6 +10,15 @@
     (is (= {:name "foo bar" :times-pinged 30}
            (initialize-person "foo bar, 30")))))
 
+(deftest find-least-pinged-test
+  (testing "Return a seq with the people with the smallest number of pings"
+    (is (= [{:name "foo bar" :times-pinged 0}
+            {:name "baz" :times-pinged 0}]
+           (find-least-pinged [{:name "foo bar"  :times-pinged 0}
+                               {:name "baz"      :times-pinged 0}
+                               {:name "John Doe" :times-pinged 3}
+                               {:name "Jane Roe" :times-pinged 39}])))))
+
 (deftest read-people-test
   (testing "Read a file and return a sequence with an array of maps"
     (is (= [{:name "foo bar"  :times-pinged 0}
